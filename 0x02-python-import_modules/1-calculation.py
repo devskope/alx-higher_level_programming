1118fd1e5af0433f1678a7c354f4bc854b163731
devskope <pmskope@gmail.com>
2023-03-12 17:00:28 +0100

    refactor tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Import calculator."""
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-
-def calculate():
-    """Use calculator functions."""
     a = 10
     b = 5
-    print(f'{a} + {b} = {add(a, b)}')
-    print(f'{a} - {b} = {sub(a, b)}')
-    print(f'{a} * {b} = {mul(a, b)}')
-    print(f'{a} / {b} = {div(a, b)}')
-
-
-if __name__ == "__main__":
-    calculate()
+    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
